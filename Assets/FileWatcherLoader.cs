930cf02fe2dae9be93a88df01b348bde276165e9
FacuBritez <FacundoIvanBritez@gmail.com>
2026-06-02 23:05:11 -0300

    Añadiendo segundo display

--- Assets/FileWatcherLoader.cs
+++ Assets/FileWatcherLoader.cs
@@ -10,6 +10,16 @@ public class FileWatcherLoader : MonoBehaviour
 
     void Start()
     {
+        Debug.Log("pantallas detectadas " + Display.displays.Length);
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+
+
+
+
+
         // Si estás en el editor, usa una carpeta en la raíz del proyecto.
         // Si es el juego exportado, creará la carpeta al lado del archivo .exe
         if (!Application.isEditor)
